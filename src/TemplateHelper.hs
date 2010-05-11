283e6b3cbcc138db9d7a6d06c66dc64fbb76fc4f
Oscar Finnsson <oscar.finnsson@gmail.com>
2010-05-11 20:35:03 +0200

    Fix for functions with arguments and different return types.

--- src/TemplateHelper.hs
+++ src/TemplateHelper.hs
@@ -14,6 +14,7 @@
 
 module TemplateHelper (
   functionExtractor,
+  functionExtractorMap,
   locationModule
 ) where
 import Language.Haskell.TH
@@ -23,6 +24,7 @@ import Text.Regex.Posix
 import Maybe
 import Language.Haskell.Exts.Extension
 
+extractAllFunctions :: String -> String-> [String]
 extractAllFunctions pattern  = 
   allMatchingFunctions pattern . parsedModule
 
@@ -32,20 +34,19 @@ parsedModule moduleCode =
       moduleOrDefault (ParseOk m) = m
   in moduleOrDefault pMod 
 
-allFunctions = onlyJust hsIdent . onlyJust hsPVar . onlyJust hsPatBind . hsModuleDecls 
+allFunctions =  onlyJust extractNameOfFunctionFromDecl . hsModuleDecls 
 allMatchingFunctions pattern = filter (\f->f=~pattern::Bool) . allFunctions 
+
+
+
+extractNameOfFunctionFromDecl :: Decl -> Maybe String
+extractNameOfFunctionFromDecl (PatBind _ (PVar (Ident n)) _ _ _ ) = Just n
+extractNameOfFunctionFromDecl (FunBind ms) = Just $ head $ [n | (Language.Haskell.Exts.Syntax.Match _ (Ident n) _ _ _ _)  <- ms]
+extractNameOfFunctionFromDecl _ = Nothing
 
 onlyJust f = map fromJust . filter isJust . map f
 
 hsModuleDecls (Module _ _ _ _ _ _ d) = d
-hsPatBind (PatBind _ p _ _ _) = Just p
-hsPatBind _ = Nothing
-
-hsPVar (PVar n) = Just n
-hsPVar _ = Nothing
-
-hsIdent (Ident n) = Just n
-hsIdent _ = Nothing
 
 -- | Extract the names and functions from the module where this function is called.
 -- 
@@ -63,6 +64,34 @@ functionExtractor pattern =
      let functions = extractAllFunctions pattern moduleCode
          makePair n = TupE [ LitE $ StringL n , VarE $ mkName n]
      return $ ListE $ map makePair functions
+
+
+-- | Extract the names and functions from the module and apply a function to every pair.
+-- 
+-- Is very useful if the common denominator of the functions is just a type class.
+--
+-- > secondTypeclassTest =
+-- >   do let expected = ["45", "88.8", "\"hej\""]
+-- >          actual = $(functionExtractorMap "^tc" [|\n f -> show f|] )
+-- >      expected @=? actual
+-- > 
+-- > tcInt :: Integer
+-- > tcInt = 45
+-- > 
+-- > tcDouble :: Double
+-- > tcDouble = 88.8
+-- > 
+-- > tcString :: String
+-- > tcString = "hej"
+functionExtractorMap :: String -> ExpQ -> ExpQ
+functionExtractorMap pattern funcName =
+  do loc <- location
+     moduleCode <- runIO $ readFile $ loc_filename loc
+     let functions :: [String]
+         functions = extractAllFunctions pattern moduleCode
+     fn <- funcName
+     let makePair n = AppE (AppE (fn) (LitE $ StringL n)) (VarE $ mkName n)
+     return $ ListE $ map makePair functions 
 
 -- | Extract the name of the current module.
 locationModule :: ExpQ
--- src/TemplateHelperTest.hs
+++ src/TemplateHelperTest.hs
@@ -26,7 +26,10 @@ groupsOfTest = [
         testGroup "Meta Group" [
           testCase "first test" firstTest,
           testCase "second text" secondTest,
-          testCase "third test" thirdTest
+          testCase "third test" thirdTest,
+          testCase "typeclassTest" typeclassTest,
+          testCase "secondTypeclassTest" secondTypeclassTest,
+          testCase "thirdTypeclassTest" thirdTypeclassTest
           ]
         ]
 
@@ -37,3 +40,35 @@ secondTest = "TemplateHelperTest" @=? $(locationModule)
 thirdTest = snd (head $(functionExtractor "thirdFunction")) @=? "hej"
 
 thirdFunction = "hej"
+
+
+
+
+typeclassTest =
+  do let expected = "thirdFunctionhej"
+         res :: [String]
+         res = $(functionExtractorMap "thirdFunction" [| \n f -> n ++ f |])
+         actual = head res
+     expected @=? actual
+
+
+secondTypeclassTest =
+  do let expected = ["45", "88.8", "\"hej\""]
+         actual = $(functionExtractorMap "^tc" [|\n f -> show f|] )
+     expected @=? actual
+
+thirdTypeclassTest =
+  do let expected = 1
+         actual = length $(functionExtractor "functionWithArgument")
+     expected @=? actual
+
+functionWithArgument xs = "hej" ++ xs
+
+tcInt :: Integer
+tcInt = 45
+
+tcDouble :: Double
+tcDouble = 88.8
+
+tcString :: String
+tcString = "hej"
